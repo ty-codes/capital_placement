a28865dc586f26a6371ce25880ee8adad8569df0
Bolatito Adeoye <titoadeoye00@gmail.com>
2023-09-29 07:46:20 -0800

    initial file upload

---
+++ prettier.config.js
@@ -0,0 +1,27 @@
+// https://prettier.io/docs/en/options.html
+
+module.exports = {
+  bracketSpacing: true,
+  bracketSameLine: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  importOrder: [
+    '^react/(.*)$',
+    '^hooks',
+    'lodash',
+    '@iconify',
+    '^layouts/(.*)$',
+    '^atoms/(.*)$',
+    '^components/(.*)$',
+    '^views/(.*)$',
+    '^store/(.*)$',
+    '^utils/(.*)$',
+    '^assets/(.*)$',
+    '^[./].*(?<!\\.(c|le|sc)ss)$',
+    '\\.(c|le|sc)ss$',
+    '^[./]',
+  ],
+  importOrderSeparation: false,
+  importOrderSortSpecifiers: true,
+};
